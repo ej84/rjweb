225bb314e1fbb029c897df98ccda85226086be7a
RjCoder <ej84@njit.edu>
2024-03-20 15:38:51 -0400

    initiated portfolio main page.

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Portfolio</h1>
+      </div>
+      <div>
+        <p>Projects</p>
+      </div>
+    </main>
+  );
+}
