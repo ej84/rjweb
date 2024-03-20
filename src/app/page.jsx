4e5e684d557d79c2dcd9ea1c5bb6c1fd72656e49
RjCoder <ej84@njit.edu>
2024-03-20 16:08:42 -0400

    Put titles for each section, changed text color.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Portfolio</h1>
+        <h1 className="text-2xl md:text-3xl text-center text-white font-bold">
+          Hello! My name is Richard and I am a Full Stack Web Developer.
+        </h1>
+      </div>
+      <div>
+        <h3>About Me</h3>
       </div>
       <div>
         <p>Projects</p>
