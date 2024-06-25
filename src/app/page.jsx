0c4e122645922aa783a67a231875967e2537c608
RjCoder <ej84@njit.edu>
2024-06-25 13:39:44 -0400

    Added image.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,12 +9,14 @@ import {
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 import Link from "next/link";
+import main from "./image/main.png";
 
 export default function Home() {
   return (
     <>
-      <nav className="flex justify-end">
+      <nav className="flex justify-end p-2">
         <Link href="https://www.github.com/ej84" className="pr-5 my-5">
           Resume
         </Link>
@@ -29,18 +31,34 @@ export default function Home() {
         </Link>
       </nav>
       <main className="flex min-h-screen flex-col justify-center lg:p-14">
-        <div className="pb-10 text-center space-y-3">
-          <h1 className="text-3xl lg:text-4xl fade-in">Hello I'm Richard,</h1>
-          <h2 className="text-lg block lg:text-2xl">
+        <div className="p-5 mb-2 text-center">
+          <h1 className="text-3xl lg:text-4xl fade-in">Hello, I'm Richard.</h1>
+        </div>
+        <div className="flex justify-center p-3">
+          <Image
+            src={main}
+            alt="main"
+            width="300"
+            height="300"
+            className="relative"
+          />
+        </div>
+        <div className="p-5 mt-2 text-center">
+          <h2 className="text-lg lg:text-2xl">
             Full Stack Web Developer and Designer.
           </h2>
         </div>
-        <div className="px-10 my-10 lg:px-48 flex justify-between text-center">
+        <div className="px-10 my-10 lg:px-48 lg:my-8 flex justify-between text-center">
           <button className="p-3 m-2">
             <a href="/" className="text-2xl">
               About Me
             </a>
           </button>
+          <div className="hidden lg:flex lg:space-x-4">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+          </div>
           <button className="p-3 m-2">
             <Link href="https://www.github.com/ej84" className="text-2xl">
               Projects
