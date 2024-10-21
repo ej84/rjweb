c5fc1bcb047164885e566b2d7d3a36701f1039c8
ej84 <ej84@njit.edu>
2024-10-21 17:36:57 -0400

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "@mui/material";
-import { motion, spring } from "framer-motion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 import main from "./image/main.png";
