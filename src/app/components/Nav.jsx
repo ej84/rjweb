5b61abd1dc387aefee9ea945eab3e4e7d08f3981
RjCoder <ej84@njit.edu>
2024-10-22 15:59:37 -0400

    Updated Nav.

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,45 +1,36 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { Home, User, FileBadge2, Github, Linkedin } from "lucide-react";
 const Nav = () => {
   return (
-    <nav className="flex justify-center p-3 text-center text-xs md:justify-end md:p-5 md:text-lg font-bold space-x-4">
-      <button className="w-12 h-12 rounded-full border-solid border-2 border-white">
-        <Link href="/" className="flex items-center justify-center">
-          <Home className="w-6 h-6 text-white" />
-        </Link>
-      </button>
-      <button className="w-12 h-12 rounded-full border-solid border-2 border-white">
-        <Link href="/about" className="flex items-center justify-center">
-          <User className="w-6 h-6 text-white" />
-        </Link>
-      </button>
+    <nav className="flex justify-center p-3 text-center text-xs md:justify-end md:p-5 md:text-lg font-bold space-x-6 relative right-5">
+      {/*<button className="w-12 h-12 rounded-full border-solid border-2 border-white">*/}
+      <Link href="/" className="flex items-center justify-center">
+        <Home className="custom-icon peer pop" />
+        <span className="absolute hidden peer-hover:block top-3/4 text-white">
+          Home
+        </span>
+      </Link>
+      <Link href="/about" className="flex items-center justify-center">
+        <User className="custom-icon pop" />
+      </Link>
 
-      <button className="w-12 h-12 rounded-full border-sollid border-2 border-white">
-        <Link href="/projects" className="flex items-center justify-center">
-          <FileBadge2 className="w-6 h-6 text-white" />
-        </Link>
-      </button>
-      <button className="w-12 h-12 rounded-full border-solid border-2 border-white">
-        <Link
-          href="https://www.github.com/ej84"
-          className="flex items-center justify-center"
-        >
-          <Github className="w-6 h-6 text-white" />
-        </Link>
-      </button>
-
-      <button className="w-12 h-12 rounded-full border-solid border-2 border-white">
-        <Link
-          href="https://www.linkedin.com/in/rjeong397/"
-          className="flex items-center justify-center"
-        >
-          <Linkedin className="w-6 h-6 text-white" />
-        </Link>
-      </button>
+      <Link href="/projects" className="flex items-center justify-center">
+        <FileBadge2 className="custom-icon pop" />
+      </Link>
+      <Link
+        href="https://www.github.com/ej84"
+        className="flex items-center justify-center"
+      >
+        <Github className="custom-icon pop" />
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/rjeong397/"
+        className="flex items-center justify-center"
+      >
+        <Linkedin className="custom-icon pop" />
+      </Link>
     </nav>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import main from "./image/main.png";
+import html from "./image/html.png";
 import Nav from "./components/Nav";
 
 export default function Home() {
@@ -16,14 +17,21 @@ export default function Home() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover"
       />
       <Nav />
-      <main className="flex flex-col justify-center lg:p-14 text-white">
+      <main className="flex justify-center lg:p-14 text-white h-screen">
         <div className="p-4 m-3 text-center lg:top-20 space-y-5">
           <h1 className="text-3xl lg:text-4xl font-extrabold fade-in">
             Hello! My name is Richard.
           </h1>
           {/*</div>
         <div className="p-3 m-2 text-center">*/}
-
+          <div className="p-5">
+            <Link
+              href="/about"
+              className="flex items-center justify-center w-20 h-24 pop"
+            >
+              <Image src={html} alt="about_me_btn" />
+            </Link>
+          </div>
           <h2 className="text-lg font-bold lg:text-4xl fade-in_2">
             I'm a <span>Full Stack Web Developer.</span>
           </h2>
