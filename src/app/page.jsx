96f08f5a43bbf2d1092946338bc229a6cfd27ac6
RjCoder <ej84@njit.edu>
2024-10-25 13:10:37 -0400

    Changed Main Image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useMediaQuery } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import main from "./image/main.png";
+import main from "./image/main.webp";
 import html from "./image/html.png";
 import Nav from "./components/Nav";
 
@@ -18,12 +18,17 @@ export default function Home() {
       />
       <Nav />
       <main className="flex justify-center lg:p-14 text-white h-screen">
-        <div className="p-4 m-3 text-center lg:top-20 space-y-5">
-          <h1 className="text-3xl lg:text-4xl font-extrabold fade-in">
-            My name is Richard.
-          </h1>
+        <div className="p-5 m-3 text-center lg:top-20 space-y-5">
+          <div className="mx-28 bg-slate-700 rounded-md ">
+            <h1 className="text-3xl lg:text-4xl font-extrabold fade-in">
+              My name is Richard.
+            </h1>
+          </div>
           {/*</div>
         <div className="p-3 m-2 text-center">*/}
+          <h2 className="text-lg font-bold lg:text-4xl fade-in_2 p-1 bg-slate-700 rounded-md">
+            I'm a <span>Full Stack Web Developer.</span>
+          </h2>
           <div className="p-5">
             <Link
               href="/about"
@@ -32,9 +37,6 @@ export default function Home() {
               <Image src={html} alt="about_me_btn" />
             </Link>
           </div>
-          <h2 className="text-lg font-bold lg:text-4xl fade-in_2">
-            I'm a <span>Full Stack Web Developer.</span>
-          </h2>
         </div>
       </main>
     </>
