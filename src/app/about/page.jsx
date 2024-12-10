2e2962c9666d169c9fc4b9afd8cf7a1f516db8ad
ej84 <ej84@njit.edu>
2024-12-09 21:10:32 -0500

    .

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -21,22 +21,18 @@ const page = () => {
           </h1>
         </div>
         <div className="p-5 text-center md:text-start">
-          <div className="float-left w-1/2">
+          <div className="float-left w-1/2 relative lg:left-36">
             <h2 className="text-lg text-white p-4 bg-slate-700 rounded-md fade-in_2">
-              My name is Richard Jeong, a full stack web application developer
-              and UI designer with a great passion for coding challenges and
-              many types of web development. I specialize in building powerful
-              web applications using React.js and Next.js for the frontend, and
-              PHP and Python for the backend, and MySQL and MongoDB for the
-              database. I thrive in collaborative environments, and I am
+              I am a full stack web application developer and UI/UX designer with a great passion for coding challenges and
+              many types of web development for product. I specialize in building powerful and scalable
+              web applications using various powerful web framework such as Next JS and powerful languages like Python, Javascript, and many others.
+              I always thrive in collaborative environments, and I am
               committed to improving user experiences through thoughtful and
-              efficient design. I look forward to growing as a developer and
-              tackling new challenges. You can explore my projects on this
-              website, and I'm always open to collaboration and inquiries—feel
-              free to reach out!
+              efficient design. I look forward to growing as more creative developer and
+              tackling new challenges!
             </h2>
           </div>
-          <div className="float-right w-1/2 pl-20">
+          <div className="float-right w-1/2 lg:pl-56">
             <img
               src="portfolio_picture.jpg"
               alt="profile"
@@ -44,12 +40,6 @@ const page = () => {
               height="380"
             />
           </div>
-        </div>
-        <div className="p-10 m-5 grid grid-cols-4 justify-center">
-          <h1>Python</h1>
-          <h1>PHP</h1>
-          <h1>React JS</h1>
-          <h1>MySQL</h1>
         </div>
       </main>
     </>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,6 @@ import react from "./image/react.png";
 import php from "./image/php.png";
 import javascript from "./image/javascript.png";
 import Nav from "./components/Nav";
-import Canvas from "./components/Canvas";
 
 export default function Home() {
   return (
@@ -23,7 +22,7 @@ export default function Home() {
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover"
       />
       <Nav />
-      <main className="flex justify-center lg:p-14 text-white h-screen">
+      <main className="flex justify-center lg:p-14 text-white">
         <div className="p-5 m-3 text-center lg:top-20 space-y-5 md:space-y-10">
           <div className="mx-28 bg-slate-700 rounded-md fade-in">
             <h1 className="text-3xl lg:text-4xl font-extrabold">
@@ -53,9 +52,6 @@ export default function Home() {
             <Link href="/about" className="flex items-center w-10 md:w-20 pop">
               <Image src={php} alt="about_me_btn" />
             </Link>
-          </div>
-          <div>
-            <Canvas />
           </div>
         </div>
       </main>
