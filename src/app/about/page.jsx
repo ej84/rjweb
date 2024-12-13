724ad30bb7aa30d54ec6fa47792925db3969cc88
RjCoder <ej84@njit.edu>
2024-12-13 15:46:04 -0500

    .

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -21,7 +21,7 @@ const page = () => {
             About Me
           </h1>
         </div>
-        <div className="p-5 text-center md:text-start">
+        {/*<div className="p-5 text-center md:text-start">
           <div className="float-left w-1/2 relative lg:left-32 xl:left-44">
             <h2 className="text-base lg:text-lg text-white p-4 bg-slate-700 rounded-md fade-in_2">
               I am a full stack web developer and UI/UX designer with high
@@ -39,8 +39,29 @@ const page = () => {
               height="320"
             />
           </div>
+        </div>*/}
+        <div className="flex justify-center w-full text-center md:text-start">
+          <div className="grid grid-cols-2 justify-center items-center text-center space-x-3">
+            <div className="max-w-80">
+              <h2 className="text-base lg:text-lg text-white p-4 bg-slate-700 rounded-md fade-in_2">
+                I am a full stack web developer and UI/UX designer with high
+                passion in coding and problem solving skill. I'm committed to
+                improving user experiences through thoughtful and efficient
+                design. I look forward to growing as more creative developer and
+                having new project challenges!
+              </h2>
+            </div>
+            <div className="w-80">
+              <img
+                src="portfolio_picture.jpg"
+                alt="profile"
+                width="320"
+                height="320"
+              />
+            </div>
+          </div>
         </div>
-        <div className="mx-auto">
+        <div className="mx-auto pt-10">
           <StackBar />
         </div>
       </main>
