b05c7239382187105ef4064d185d83d708ee8cf0
RjCoder <ej84@njit.edu>
2024-12-16 15:16:24 -0500

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -25,12 +25,12 @@ export default function Home() {
       <Nav />
       <main className="flex justify-center lg:p-14 text-white">
         <div className="p-5 m-3 text-center lg:top-20 space-y-5 md:space-y-10">
-          <div className="mx-28 bg-slate-700 rounded-md fade-in">
-            <h1 className="p-3 text-3xl lg:text-4xl font-extrabold">
-              My name is Richard.
+          <div className="mx-5 bg-slate-700 rounded-md fade-in">
+            <h1 className="p-3 text-2xl lg:text-3xl font-extrabold">
+              I'm Richard Jeong.
             </h1>
           </div>
-          <h2 className="text-xl font-bold lg:text-4xl fade-in_2 p-1 bg-slate-700 rounded-md">
+          <h2 className="text-xl font-bold lg:text-3xl fade-in_2 p-1 bg-slate-700 rounded-md">
             I'm a <span>Full Stack Web Developer.</span>
           </h2>
         </div>
