dc8de313315a4a5c12789221d261ad115af0b941
RjCoder <ej84@njit.edu>
2024-12-16 12:49:00 -0500

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,14 +26,13 @@ export default function Home() {
       <main className="flex justify-center lg:p-14 text-white">
         <div className="p-5 m-3 text-center lg:top-20 space-y-5 md:space-y-10">
           <div className="mx-28 bg-slate-700 rounded-md fade-in">
-            <h1 className="text-3xl lg:text-4xl font-extrabold">
+            <h1 className="p-3 text-3xl lg:text-4xl font-extrabold">
               My name is Richard.
             </h1>
           </div>
-          <h2 className="text-lg font-bold lg:text-4xl fade-in_2 p-1 bg-slate-700 rounded-md">
+          <h2 className="text-xl font-bold lg:text-4xl fade-in_2 p-1 bg-slate-700 rounded-md">
             I'm a <span>Full Stack Web Developer.</span>
           </h2>
-          <StackBar />
         </div>
       </main>
     </>
