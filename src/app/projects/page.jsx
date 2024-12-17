d46c36f9bfaea10ce2425b6f67ffd0cdc01f3527
RjCoder <ej84@njit.edu>
2024-12-17 16:01:42 -0500

    .

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -15,43 +15,46 @@ const page = () => {
         </div>
         <div className="p-4 m-5 md:p-1 md:m-1">
           <div className="grid grid-rows-3 md:grid-cols-2 justify-center gap-3 text-center">
-            <div className="">
+            <div className="place-items-center">
               <h2 className="text-style">File Converter Web Application</h2>
-              <div className="md:p-4 md:m-4 text-style space-y-3">
-                <div className="">
-                  <a href="https://github.com/ej84/all-converter">
-                    <Image
-                      src={converter}
-                      alt="file converter"
-                      width={140}
-                      height={200}
-                    />
-                  </a>
+              <div className="p-4 m-4 text-style">
+                <div className="flex">
+                  <div className="mt-2 md:pr-2 rounded-md">
+                    <a href="https://github.com/ej84/all-converter">
+                      <Image
+                        src={converter}
+                        alt="file converter"
+                        width={265}
+                        height={200}
+                      />
+                    </a>
+                  </div>
+                  <div className="p-2 mt-2 md:mt-4 w-full h-36 rounded-md bg-white text-slate-700 space-y-3">
+                    <p className="text-xs md:text-base">
+                      An online file converter SaaS app that helps edit, modify
+                      and create new format files such as image png to webp
+                      file, video mp4 to audio mp3, etc.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div>
-              <h2 className="text-style">
-                File Converter Web Application Project
-              </h2>
+            <div className="place-items-center">
+              <h2 className="text-style">IT HelpDesk Ticketing System</h2>
               <div className="md:p-10 md:m-4 space-y-3 text-style">
                 <a href="https://github.com/ej84/all-converter"></a>
                 <p> </p>
               </div>
             </div>
-            <div>
-              <h2 className="text-style">
-                File Converter Web Application Project
-              </h2>
+            <div className="place-items-center">
+              <h2 className="text-style">Project 3</h2>
               <div className="md:p-10 md:m-4 space-y-3 text-style">
                 <a href="https://github.com/ej84/all-converter"></a>
                 <p> </p>
               </div>
             </div>
-            <div>
-              <h2 className="text-style">
-                File Converter Web Application Project
-              </h2>
+            <div className="place-items-center">
+              <h2 className="text-style">Project 4</h2>
               <div className="md:p-10 md:m-4 space-y-3 text-style">
                 <a href="https://github.com/ej84/all-converter"></a>
                 <p> </p>
